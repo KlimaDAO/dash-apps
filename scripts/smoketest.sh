7681aaac939c93ad14610e3835fa3d516c27e598
Marcus Aurelius <marcus_aurelius_0x@protonmail.com>
2023-02-03 11:06:25 -0500

    Add better logging to smoketest

--- scripts/smoketest.sh
+++ scripts/smoketest.sh
@@ -4,15 +4,21 @@ FAILING=0
 
 for a in src/apps/tco2_dashboard/app.py; do
   module="${a////.}"
-  python -m ${module%.py} &
+  nohup python -m ${module%.py} &
   DASH_PID=$!
   sleep 30
-  RESP_CODE=$(curl --head --location --write-out %{http_code} --silent --output /dev/null http://127.0.0.1:8050/)
+  RESP_CODE=$(curl --head --location --write-out %{http_code} --silent --output response.txt http://127.0.0.1:8050/)
   echo $RESP_CODE
   kill `lsof -w -n -i tcp:8050 | awk '$2!="PID" {print $2;}'`
   if [ "$RESP_CODE" != "200" ];
   then
     echo "$a FAILED! ($RESP_CODE)"
+    echo "--------------"
+    echo "Server Logs:"
+    cat nohup.out
+    echo "--------------"
+    echo "UI response:"
+    cat response.txt
     FAILING=1
   else
     echo "$a OK ($RESP_CODE)"
