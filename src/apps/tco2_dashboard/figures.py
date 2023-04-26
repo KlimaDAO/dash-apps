c1bdec102bdc97338d611f4f11736b4e5b82b1a5
StatCityPro <attwood1910@gmail.com>
2023-04-26 23:17:46 +0100

    update pool_klima_retirement_table

--- src/apps/tco2_dashboard/figures.py
+++ src/apps/tco2_dashboard/figures.py
@@ -1893,14 +1893,18 @@ def pool_klima_retirement_table(summary_table):
           "presentation": "markdown",
           "type": "text"} for i in summary_table.columns],
         style_header={"backgroundColor": GRAY,
-                      'color': WHITE,
-                      "text-align": "center"},
+                      "text-align": "center", 
+                      "color": WHITE,
+                      "font_size": "10px", 
+                      "font-family": 'Inter, sans-serif',
+                      "fontWeight": "bold"},
         style_data={
             "backgroundColor": DARK_GRAY,
-            "color": WHITE},
+            "color": WHITE,  
+            "font_size": "10px", 
+            "text-align": "center", 
+            'font-family': 'Inter, sans-serif'},
         style_table={
-            "overflowX": "auto"},
-        style_cell={
-            'fontSize': 11,
-            'font-family': 'sans-serif'})
+            "overflowX": "auto"}
+    )
     return table
