72005b34d1bf9b9f65df1cf35af7cd7463b9325f
StatCityPro <attwood1910@gmail.com>
2023-05-10 22:25:54 +0100

    pandas removed as no longer used

--- src/apps/tco2_dashboard/retirement_trends/impl/retirement_trends_pool.py
+++ src/apps/tco2_dashboard/retirement_trends/impl/retirement_trends_pool.py
@@ -11,7 +11,6 @@ from src.apps.tco2_dashboard.retirement_trends.retirement_trends_types \
     import ChartContent, ListData, TopContent
 import dash_bootstrap_components as dbc
 from dash import html, dcc
-import pandas as pd
 import numpy as np
 
 
