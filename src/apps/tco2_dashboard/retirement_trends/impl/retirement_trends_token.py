85e3d53af37ff9fa6f9513eb23ae2562a5f35de1
StatCityPro <attwood1910@gmail.com>
2023-05-16 22:26:54 +0100

    add numpy package

--- src/apps/tco2_dashboard/retirement_trends/impl/retirement_trends_token.py
+++ src/apps/tco2_dashboard/retirement_trends/impl/retirement_trends_token.py
@@ -5,6 +5,7 @@ from src.apps.tco2_dashboard.retirement_trends.retirement_trends_types \
     import ChartContent, ListData, TopContent
 import dash_bootstrap_components as dbc
 from dash import html, dcc
+import numpy as np
 
 
 class RetirementTrendsByToken(RetirementTrendsInterface):
