93d7d0aa12edc121ae2786426071a5415a640896
biwano <biwano@gmail.com>
2023-06-02 21:13:15 +0200

    Removed column renaming

--- src/apps/tco2_dashboard/constants.py
+++ src/apps/tco2_dashboard/constants.py
@@ -32,124 +32,16 @@ USDC_DECIMALS = 12
 
 BETWEEN_SECTION_STYLE = {"padding-top": "20px"}
 
-rename_map = {
-    "carbonOffsets_bridges_value": "Quantity",
-    "carbonOffsets_bridges_timestamp": "Date",
-    "carbonOffsets_bridge": "Bridge",
-    "carbonOffsets_region": "Region",
-    "carbonOffsets_vintage": "Vintage",
-    "carbonOffsets_projectID": "Project ID",
-    "carbonOffsets_standard": "Standard",
-    "carbonOffsets_methodology": "Methodology",
-    "carbonOffsets_country": "Country",
-    "carbonOffsets_category": "Project Type",
-    "carbonOffsets_name": "Name",
-    "carbonOffsets_tokenAddress": "Token Address",
-    "carbonOffsets_balanceBCT": "BCT Quantity",
-    "carbonOffsets_balanceNCT": "NCT Quantity",
-    "carbonOffsets_balanceUBO": "UBO Quantity",
-    "carbonOffsets_balanceNBO": "NBO Quantity",
-    "carbonOffsets_totalBridged": "Total Quantity",
-}
-
-mco2_bridged_rename_map = {
-    "batches_id": "ID",
-    "batches_serialNumber": "Serial Number",
-    "batches_timestamp": "Date",
-    "batches_tokenAddress": "Token Address",
-    "batches_vintage": "Vintage",
-    "batches_projectID": "Project ID",
-    "batches_value": "Quantity",
-    "batches_originaltx": "Original Tx Address",
-}
-
-retires_rename_map = {
-    "retires_value": "Quantity",
-    "retires_timestamp": "Date",
-    "retires_retiree": "Retiree",
-    "retires_offset_bridge": "Bridge",
-    "retires_offset_region": "Region",
-    "retires_offset_vintage": "Vintage",
-    "retires_offset_projectID": "Project ID",
-    "retires_offset_standard": "Standard",
-    "retires_offset_methodology": "Methodology",
-    "retires_offset_country": "Country",
-    "retires_offset_category": "Project Type",
-    "retires_offset_name": "Name",
-    "retires_offset_tokenAddress": "Token Address",
-    "retires_offset_totalRetired": "Total Quantity",
-    "retires_transaction_id": "Tx ID",
-    "retires_transaction_from": "Tx From Address",
-}
-
-moss_retires_rename_map = {
-    "mossOffsets_value": "Quantity",
-    "mossOffsets_timestamp": "Date",
-    "mossOffsets_retiree": "Retiree",
-    "mossOffsets_receiptId": "Receipt ID",
-    "mossOffsets_onBehalfOf": "OnBehalf Of",
-    "mossOffsets_transaction_id": "Tx ID",
-    "mossOffsets_transaction_from": "Tx From Address",
-}
-
-bridges_rename_map = {
-    "bridges_value": "Quantity",
-    "bridges_timestamp": "Date",
-    "bridges_transaction_id": "Tx Address",
-}
 
 
-redeems_rename_map = {
-    "redeems_value": "Quantity",
-    "redeems_timestamp": "Date",
-    "redeems_pool": "Pool",
-    "redeems_offset_region": "Region",
-}
 
-deposits_rename_map = {
-    "deposits_value": "Quantity",
-    "deposits_timestamp": "Date",
-    "deposits_pool": "Pool",
-    "deposits_offset_region": "Region",
-}
 
-pool_retires_rename_map = {
-    "klimaRetires_amount": "Quantity",
-    "klimaRetires_timestamp": "Date",
-    "klimaRetires_pool": "Pool",
-    "klimaRetires_retiringAddress": "Retiring Address",
-    "klimaRetires_beneficiary": "Beneficiary",
-    "klimaRetires_beneficiaryAddress": "Beneficiary Address",
-    "klimaRetires_retirementMessage": "Retirement Message",
-    "klimaRetires_transaction_id": "Tx ID",
-}
 
-verra_rename_map = {
-    "issuanceDate": "Issuance Date",
-    "programObjectives": "Sustainable Development Goals",
-    "instrumentType": "Credit Type",
-    "vintageStart": "Vintage Start",
-    "vintageEnd": "Vintage End",
-    "reportingPeriodStart": "Reporting Period Start",
-    "reportingPeriodEnd": "Reporting Period End",
-    "resourceIdentifier": "ID",
-    "resourceName": "Name",
-    "region": "Region",
-    "country": "Country",
-    "protocolCategory": "Project Type",
-    "protocol": "Methodology",
-    "totalVintageQuantity": "Total Vintage Quantity",
-    "quantity": "Quantity Issued",
-    "serialNumbers": "Serial Number",
-    "additionalCertifications": "Additional Certifications",
-    "retiredCancelled": "Is Cancelled",
-    "retireOrCancelDate": "Retirement/Cancellation Date",
-    "retirementBeneficiary": "Retirement Beneficiary",
-    "retirementReason": "Retirement Reason",
-    "retirementDetails": "Retirement Details",
-    "inputTypes": "Input Type",
-    "holdingIdentifier": "Holding ID",
-}
+
+
+
+
+
 
 merge_columns = [
     "ID",
@@ -195,14 +87,7 @@ mco2_verra_rename_map = {
 }
 
 
-holders_rename_map = {
-    "holdings_id": "ID",
-    "holdings_token": "Token",
-    "holdings_timestamp": "Date",
-    "holdings_tokenAmount": "Quantity",
-    "holdings_carbonValue": "CarbonValue",
-    "holdings_klimate_id": "Klimate_Address",
-}
+
 
 GRAPH_FONT = dict(size=8, color="white", family="Inter, sans-serif")
 PIE_CHART_FONT = dict(size=12, color="white", family="Inter, sans-serif")
