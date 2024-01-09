ce4ca54d9b98789de72b3958ec0503d44db10122
biwano <biwano@gmail.com>
2024-01-09 16:29:19 +0100

    Use a short cache for tokens data

--- src/apps/api/endpoints/tokens.py
+++ src/apps/api/endpoints/tokens.py
@@ -1,10 +1,10 @@
 from flask_restful import Resource
-from src.apps.services import Tokens as Service, layout_cache
+from src.apps.services import Tokens as Service, services_short_cache
 from . import helpers
 
 
 class Tokens(Resource):
-    @layout_cache.cached(query_string=True)
+    @services_short_cache.cached(query_string=True)
     @helpers.with_errors_handler
     @helpers.with_help(
         f"""Get tokens information
