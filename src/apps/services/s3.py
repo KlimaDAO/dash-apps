2589687588f5aeb897af2b46f51e5043dc5b0951
biwano <biwano@gmail.com>
2024-02-26 10:30:18 +0100

    Fixes tokens cache

--- src/apps/services/s3.py
+++ src/apps/services/s3.py
@@ -1,8 +1,12 @@
 from ...util import load_s3_data
-from . import KeyCacheable, single_cached_command
+from . import KeyCacheable, single_cached_command, services_long_cache
 
 
 class S3(KeyCacheable):
+    """Service for offsets"""
+    def __init__(self, commands=[], cache=services_long_cache):
+        super(S3, self).__init__(commands, cache)
+
     @single_cached_command()
     def load(self, slug):
         return load_s3_data(slug)
